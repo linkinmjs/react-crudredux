d3570970d485560df63526630ab5eed11c677c08
Mauri <mauricio.stampella@25watts.com.ar>
2021-03-31 11:16:13 -0300

    Creando el Action para Eliminar

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Redux
+import { useDispatch } from 'react-redux';
+import { borrarProductoAction } from '../actions/productoActions';
+
 const Producto = ({producto}) => {
 
     const {nombre, precio, id} = producto;
+
+    const dispatch = useDispatch();
+
+    // confirmar si desea eliminarlo
+    const confirmarEliminarProducto = id => {
+        // preguntar al usuario
+
+        // pasarlo al action
+        dispatch( borrarProductoAction (id) );
+    }
 
     return ( 
         <tr>
@@ -16,6 +30,7 @@ const Producto = ({producto}) => {
                 <button 
                     type="button"
                     className="btn btn-danger"
+                    onClick={() => confirmarEliminarProducto(id)}
                 >Eliminar</button>
             </td>
         </tr>
