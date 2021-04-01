68965062e30429ecc55ed5438e187f7540ea9248
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-01 12:28:38 -0300

    Creando un segundo Reducer

---
+++ src/reducers/alertaReducer.js
@@ -0,0 +1,19 @@
+import{
+    MOSTRAR_ALERTA,
+    OCULTAR_ALERTA
+} from '../types'
+
+// cada reducer tiene su state
+const initialState = {
+    alerta: null
+}
+
+export default function(state = initialState, action){
+    switch(action.type){
+
+        
+
+        default:
+            return state;
+    }
+}
