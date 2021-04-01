9b550d6c8adafdf0a7809d44883486ccbb42ea43
Mauri <mauricio.stampella@25watts.com.ar>
2021-04-01 11:32:53 -0300

    Llenando el formulario de edición

--- src/components/EditarProducto.js
+++ src/components/EditarProducto.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 const EditarProducto = () => {
+
+    // producto a editar
+    const producto = useSelector(state => state.productos.productoeditar);
+    if(!producto) return null;
+    const {nombre, precio, id} = producto;
+
     return ( 
         <div className="row justify-content-center">
             <div className="col-md-8">
@@ -17,6 +25,7 @@ const EditarProducto = () => {
                                     className="form-control"
                                     placeholder="Nombre Producto"
                                     name="nombre"
+                                    value={nombre}
                                 />
                             </div>
                             <div className="form-group">
@@ -26,6 +35,7 @@ const EditarProducto = () => {
                                     className="form-control"
                                     placeholder="Precio Producto"
                                     name="precio"
+                                    value={precio}
                                 />
                             </div>
                             <button
